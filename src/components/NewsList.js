f00cd826dfdf15048387b01f7cb9f496ad909bb5
Swastik <swastikmohapatra01@gmail.com>
2023-06-30 17:18:44 +0530

    first commit

---
+++ src/components/NewsList.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { NewsItem } from '.'
+
+
+const NewsList = () => {
+    const [articles, setArticles] = useState([])
+
+    useEffect(() => {
+        const getArticles = async () => {
+            const response = await axios.get(`https://newsapi.org/v2/everything?q=football&apiKey=82aa007df77a4bc19a8578035b2d6037`)
+            setArticles(response.data.articles)
+            console.log(response)
+        }
+
+        getArticles()
+    }, [])
+    return (
+        <div>
+            {articles.map(article => {
+                return(
+                    <NewsItem 
+                        title={article.title}
+                        description={article.description}
+                        url={article.url}
+                        urlToImage={article.urlToImage} 
+                    />
+                )
+            })}
+        </div>
+    )
+}
+
+export default NewsList
